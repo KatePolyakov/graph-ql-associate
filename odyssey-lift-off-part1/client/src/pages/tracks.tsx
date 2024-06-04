b60f5381eff8b72a55818a4e6c16db6c079897c3
Kate <polyakovkate@gmail.com>
2024-06-04 11:45:20 -0700

    Basics. DEFINING A QUERY

--- odyssey-lift-off-part1/client/src/pages/tracks.tsx
+++ odyssey-lift-off-part1/client/src/pages/tracks.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { Layout } from "../components";
+import { gql } from "../__generated__/";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+
+/** TRACKS query to retrieve all tracks */
+const TRACKS = gql(`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`);
+
 const Tracks = () => {
   return <Layout grid> </Layout>;
 };
